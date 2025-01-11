cdfe71394e2cbfcc6c0831fc22102c0f3cad187b
Santt4naDev <jhonnsantt4na@gmail.com>
2025-01-11 14:45:16 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/santt4na/workshop_mongodb/config/Instantiation.java
+++ src/main/java/com/santt4na/workshop_mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.santt4na.workshop_mongodb.domain.Post;
 import com.santt4na.workshop_mongodb.domain.User;
+import com.santt4na.workshop_mongodb.dto.AuthorDTO;
 import com.santt4na.workshop_mongodb.repository.PostRepository;
 import com.santt4na.workshop_mongodb.repository.UserRepository;
 
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
       User alex = new User(null, "Alex Green", "alex@gmail.com");
       User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-      Post post1 = new Post(null, sdf.parse("21/03/2025"), "Partiu viagem", "Vou viajar para São Paulo,Abraços!",
-            maria);
-      Post post2 = new Post(null, sdf.parse("20/07/2028"), "Bom dia", "Acordei feliz hoje !", maria);
-
       userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+      Post post1 = new Post(null, sdf.parse("21/03/2025"), "Partiu viagem", "Vou viajar para São Paulo,Abraços!",
+            new AuthorDTO(maria));
+      Post post2 = new Post(null, sdf.parse("20/07/2028"), "Bom dia", "Acordei feliz hoje !", new AuthorDTO(maria));
+
       postRepository.saveAll(Arrays.asList(post1, post2));
    }
 
